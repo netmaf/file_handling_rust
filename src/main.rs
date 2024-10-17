1aebb17f840a6198227bd40cdcfc5436b03931c5
netmaf <>
2024-10-17 17:04:48 +0530

    demo main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-// create new file and write bytes to it
 
-use std::{fs::File, io::{Result, Write}};
 
-fn main() -> Result<()>{
-
-    let mut file = File::create("tasks.txt")?;
-    file.write_all(b"Hello")?;
-    println!("{:?}", b"Hello".to_vec());
-
-    Ok(())
+fn main() {
+    println!("hello")
 }
